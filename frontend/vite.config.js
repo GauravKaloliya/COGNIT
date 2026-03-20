d1c0f88a3a3133a77cafd9b7d356e80011c2118d
GAURAV's MAC <kaps@GAURAVs-MacBook-Air-2.local>
2026-03-20 21:59:38 +0530

    Enable production sourcemaps for debugging

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       outDir: "dist",
-      sourcemap: mode === "development",
+      sourcemap: true,
       minify: "terser",
       cssMinify: true,
       rollupOptions: {
