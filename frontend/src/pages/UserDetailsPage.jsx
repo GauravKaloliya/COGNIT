026cec2587284eb55ea57c755be3e3a3db0fb72b
GAURAV's MAC <kaps@GAURAVs-MacBook-Air-2.local>
2026-03-23 11:57:46 +0530

    Extract RefreshIcon component

--- frontend/src/pages/UserDetailsPage.jsx
+++ frontend/src/pages/UserDetailsPage.jsx
@@ -7,34 +7,7 @@ import { runtimeConfig } from "../config/runtime";
 import PageStatusBanners from "../components/PageStatusBanners.jsx";
 import ButtonRetryBadge from "../components/ButtonRetryBadge.jsx";
 import PageActions from "../components/PageActions.jsx";
-
-function RefreshIcon({ size = 18 }) {
-  return (
-    <svg
-      width={size}
-      height={size}
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      aria-hidden="true"
-      focusable="false"
-    >
-      <path
-        d="M20 12a8 8 0 1 1-2.34-5.66"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-      />
-      <path
-        d="M20 4v6h-6"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-}
+import RefreshIcon from "../components/icons/RefreshIcon.jsx";
 
 export default function UserDetailsPage({
   publicId,
